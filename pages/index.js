c08aebe467b18c3efda9b0031d27b59dfeefa3ab
Hubert <erwan.marques@yahoo.fr>
2022-06-17 19:18:13 +0200

    remove review

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,9 @@ const SectionStyle = styled.div`
 
 export default function Home() {
   return (
-<SectionStyle>
-<div>
+    <>
       < Hero/>
       <Infos/>
-      <h2 className='sectionTitle'>Vos <span>avis</span> et <span>ressenties</span> comptes</h2>
-        <div className='containerReview'>
-        <Review srcPhoto="/logo.svg" nom="Jérémy Ferrier" statut="Grand-Père" testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore" />
-        <Review srcPhoto="/logo.svg" nom="Marine Tourret" statut="Grand-Mère" testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
-        <Review srcPhoto="/logo.svg" nom="Jimmy Renden" statut="Grand-Père" testimony="Lorem ipsum dolor sit amet" />
-        </div>
-    </div>
-</SectionStyle>
+    </>
   )
 }
