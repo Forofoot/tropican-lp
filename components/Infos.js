fbe5468fda7249bd3d70672090cc67f1a7b698f3
Hubert <erwan.marques@yahoo.fr>
2022-06-24 17:08:37 +0200

    add environment variable

--- components/Infos.js
+++ components/Infos.js
@@ -76,23 +76,7 @@ const InfoStyle = styled.section`
     }
 `
 
-export default function Infos() {
-    let infos =[ 
-        {
-            title : 'Qui sommes-nous ?',
-            description : "Leste est le meilleur dispositif qui a pour but de créer des voyages et des activités entre grands-parents et petits-enfants pour renfoncer leurs formidable liens."
-        },
-        {
-            title : 'Comment ?',
-            description : "Leste vous permets d’organiser des voyages et des activités extraordinaires en France, entre grands-parents et petits-enfants tout en ayant la possibilité de les personnaliser."
-        },
-        {
-            title : 'Avec qui ?',
-            description : "Grâce à nos merveilleux partenaires et nos équipes prodigieuses nous vous accompagnons tout au long de vos voyages et/ou vos activités."
-        }
-        
-    ]
-    
+export default function Infos({ infos }) {
     useEffect(() => {
         let infos = document.querySelectorAll('[data-info]')
         let slideIndicator = document.querySelectorAll('[data-slide-indicator]')
