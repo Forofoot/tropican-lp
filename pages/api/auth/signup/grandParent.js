a8952a561f59c5ffe0296d4feb4c67a73d7a0732
Hubert <erwan.marques@yahoo.fr>
2022-07-02 23:08:08 +0200

    Inscription

---
+++ pages/api/auth/signup/grandParent.js
@@ -0,0 +1,44 @@
+import { hash } from 'bcryptjs';
+import { PrismaClient } from '@prisma/client'
+
+export default async function handler(
+    req, res
+) {
+
+    const prisma = new PrismaClient();
+    //Only POST mothod is accepted
+    if (req.method === 'POST') {
+        //Getting email and password from body
+        const { firstName, lastName, pseudo, email, password } = req.body
+        //Validate
+        if (!firstName || !lastName || !pseudo || !email || !password) {
+            res.status(422).json({ message: 'Invalid Data' })
+            return
+        }
+
+        const exist = await prisma.grandchildren.findFirst({
+            where:{
+                email
+            }
+        })
+        //Hash password
+        if(exist){
+            res.status(500).json({ message: 'Existe déjà'})
+        }
+         const status = await prisma.grandparent.create({
+            data:{
+                firstName,
+                lastName,
+                email,
+                pseudo,
+                password: await hash(password, 12),
+            }
+        })
+        //Send success response
+        res.status(201).json({ message: 'User created', ...status });
+        //Close DB connection
+    } else {
+        //Response for other than POST method
+        res.status(500).json({ message: 'Route not valid' });
+    }
+}
--- pages/experience/login/index.js
+++ pages/experience/login/index.js
@@ -1,7 +1,125 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useState } from 'react'
 
-export default function index() {
+const AuthStyle = styled.section`
+    width: 100%;
+    max-width: 800px;
+    height: 100vh;
+    margin: auto;
+    h1{
+        text-align: center;
+        margin-bottom: 50px;
+    }
+    .buttonBlock{
+        display: flex;
+        justify-content: center;
+        gap: 30px;
+        .loginButton{
+            background-color: grey;
+            padding: 30px;
+            width: 320px;
+            text-align: center;
+        }
+    }
+    
+`
+export default function Index() {
+  const [loginChoice, setLoginChoice] = useState(null)
+  
+  const [inputedGrandParent, setInputedGrandParent] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    pseudo: "",
+    password: "",
+  })
+
+  const [inputedGrandChildren, setInputedGrandChildren] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    pseudo: "",
+    password: "",
+  })
+
+  const handleCreateGrandParent = async (e) => {
+    e.preventDefault();
+    if (!inputedGrandParent.email || !inputedGrandParent.email.includes('@') || !inputedGrandParent.password || !inputedGrandParent.firstName || !inputedGrandParent.lastName || !inputedGrandParent.pseudo) {
+        alert('Invalid details');
+        return;
+    }
+    //POST form values
+    const res = await fetch('/api/auth/signup/grandParent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: inputedGrandParent.email,
+            firstName: inputedGrandParent.firstName,
+            lastName: inputedGrandParent.lastName,
+            pseudo: inputedGrandParent.pseudo,
+            password: inputedGrandParent.password,
+        }),
+    });
+  };
+
+  const handleCreateGrandChildren = async (e) => {
+    e.preventDefault();
+    if (!inputedGrandChildren.email || !inputedGrandChildren.email.includes('@') || !inputedGrandChildren.password || !inputedGrandChildren.firstName || !inputedGrandChildren.lastName  || !inputedGrandChildren.pseudo ) {
+        alert('Invalid details');
+        return;
+    }
+    //POST form values
+    const res = await fetch('/api/auth/signup/grandChildren', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: inputedGrandChildren.email,
+            firstName: inputedGrandChildren.firstName,
+            lastName: inputedGrandChildren.lastName,
+            pseudo: inputedGrandChildren.pseudo,
+            password: inputedGrandChildren.password,
+        }),
+    });
+  };
+
   return (
-    <div>test</div>
+    <AuthStyle>
+        <h1>Vous êtes ?</h1>
+        <div className='buttonBlock'>
+            <div className='loginButton' onClick={() => setLoginChoice('grandChildren')}>
+                Petit-enfant
+            </div>
+            <div className='loginButton' onClick={() => setLoginChoice('grandParent')}>
+                Grand-parent
+            </div>
+        </div>
+        
+        { loginChoice == 'grandChildren' &&
+            <form method='POST' onSubmit={handleCreateGrandChildren}>
+            <input type="text" value={inputedGrandChildren.email || ""} placeholder='email' onChange={(e) => setInputedGrandChildren({ ...inputedGrandChildren, email:e.target.value })}/>
+            <input type="text" value={inputedGrandChildren.firstName || ""} placeholder='Prénom' onChange={(e) => setInputedGrandChildren({ ...inputedGrandChildren, firstName:e.target.value })}/>
+            <input type="text" value={inputedGrandChildren.lastName || ""} placeholder='Nom' onChange={(e) => setInputedGrandChildren({ ...inputedGrandChildren, lastName:e.target.value })}/>
+            <input type="text" value={inputedGrandChildren.pseudo || ""} placeholder='Pseudo' onChange={(e) => setInputedGrandChildren({ ...inputedGrandChildren, pseudo:e.target.value })}/>
+            <input type="text"  value={inputedGrandChildren.password || ""} placeholder='Mot de passe' onChange={(e) => setInputedGrandChildren({ ...inputedGrandChildren, password:e.target.value })}/>
+            <button type='submit'>Submit</button>
+        </form>
+        }
+
+        { loginChoice == 'grandParent' &&
+            <form method='POST' onSubmit={handleCreateGrandParent}>
+                <input type="text" value={inputedGrandParent.email || ""} placeholder='email' onChange={(e) => setInputedGrandParent({ ...inputedGrandParent, email:e.target.value })}/>
+                <input type="text" value={inputedGrandParent.firstName || ""} placeholder='Prénom' onChange={(e) => setInputedGrandParent({ ...inputedGrandParent, firstName:e.target.value })}/>
+                <input type="text" value={inputedGrandParent.lastName || ""} placeholder='Nom' onChange={(e) => setInputedGrandParent({ ...inputedGrandParent, lastName:e.target.value })}/>
+                <input type="text" value={inputedGrandParent.pseudo || ""} placeholder='Pseudo' onChange={(e) => setInputedGrandParent({ ...inputedGrandParent, pseudo:e.target.value })}/>
+                <input type="text"  value={inputedGrandParent.password || ""} placeholder='Mot de passe' onChange={(e) => setInputedGrandParent({ ...inputedGrandParent, password:e.target.value })}/>
+                <button type='submit'>Submit</button>
+            </form>
+        }
+    </AuthStyle>
   )
 }
