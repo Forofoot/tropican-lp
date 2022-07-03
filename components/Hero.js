b8f44a2d20e16348b0af52e5753123a009373734
Yarline <yarlinecineus@outlook.fr>
2022-07-03 17:24:34 +0200

    landing page changement + js (à revoir)

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import Script from 'next/script'
+
+<Script
+ dangerouslySetInnerHTML={{
+    __html: ` 
+    document.addEventListener("DOMContentLoaded", function () {
+        console.log('chargé');
+        document.getElementsByClassName('star').addEventListener("mouseenter", function( event ) {
+            console.log('1');
+            var id = (event.target.id);
+            document.querySelector(".image_banner img").src="banner_over_".id.".webp";
+        })
+        document.getElementsByClassName('star').addEventListener("mouseleave", function( event ) {
+            console.log('2');
+            document.querySelector(".image_banner img").src="/hero/image_banner.webp";
+        })
+      });
+        
+        `,  
+ }}
+/>
 
 const HeroStyle = styled.section`
     position: relative;
@@ -10,8 +32,21 @@ const HeroStyle = styled.section`
     justify-content: center;
     height: 95vh;
     padding: 0 50px;
+
     .heroBanner{
         margin: 20px 0;
+        border-radius: 10px;
+        overflow:hidden;
+        position:relative;
+        width:90vw;
+        height:50vh;
+    }
+    .star{
+        display:none;
+    }
+    .absolute_banner{
+        text-align:center;
+        margin-bottom:50px;
     }
     .containerInfo{
         h1{
@@ -30,6 +65,9 @@ const HeroStyle = styled.section`
     }
     .btnPrimary{
         margin-bottom: 40px;
+    }
+    .btn{
+        padding: 15px 30px;
     }
     .containerScroll{
         display: flex;
@@ -56,11 +94,62 @@ const HeroStyle = styled.section`
             }
         }
     }
+    @media (min-width: 768px){
+        .containerScroll{
+            position:absolute;
+            color:#fff;
+            bottom:50px;
+
+            .scrollIndicator{
+                border-color: #fff;
+                .scrollIndicator{
+                    background-color:#fff;
+                }
+            }
+        }
+        .heroBanner{
+            margin: 0;
+            border-radius: 0;
+            width:100vw;
+            height:calc(100vh);
+        }
+        .absolute_banner{
+            position: absolute;
+            top:20%;
+        }
+
+        .star{
+            display:block;
+            z-index:2;
+            position: absolute;
+
+            :hover{
+                cursor:pointer;
+            }
+        }
+
+        #over_1{
+            top: 30px;
+            right:100px;
+            fill: #147543;
+        }
+        #over_2{
+            top: 170px;
+            right:120px;
+            fill: #F20D97;
+        }
+        #over_3{
+            top: 150px;
+            left:100px;
+            fill: #212F89;
+        }
+    }
 
     @media (max-width:768px){
         justify-content: start;
         height: inherit;
         padding: 0 20px ;
+
         h1{
             font-size: 2em;
             margin-bottom: 25px;
@@ -88,16 +177,45 @@ const HeroStyle = styled.section`
 const Hero = () => {
     return (
         <HeroStyle id="section1">
+            <div className='star' id="over_1">
+                <Image
+                    src={"/star.svg"}
+                    alt="étoile"
+                    width='35px'
+                    height='35px'
+                    fill='#147543'
+                />
+                </div>
+                <div className='star' id="over_2">
+                <Image
+                    src={"/star.svg"}
+                    alt="étoile"
+                    width='20px'
+                    height='20px'
+                    fill='#147543'
+                />
+                </div>
+                <div className='star' id="over_3">
+                <Image
+                    src={"/star.svg"}
+                    alt="étoile"
+                    width='25px'
+                    height='25px'
+                    fill='#147543'
+                />
+                </div>
             <div className='heroBanner'>
                 <Image
                     src={"/hero/image_banner.webp"}
                     alt="Image banner d'une cabane dans un arbre"
-                    layout='raw'
-                    width='350px'
+                    layout='fill'
+                    width='100%'
                     height='350px'
+                    objectFit='cover'
                     priority
                 />
             </div>
+            <div className="absolute_banner">
             <div className='containerInfo'>
                 <h1>Vivez des expériences,<br></br>exceptionnelles</h1>
                 <p>Nous vous aidons à transcender vos liens</p>
@@ -109,12 +227,14 @@ const Hero = () => {
                     </a>
                 </Link>
             </div>
+            </div>
             <div className='containerScroll'>
                 <div className='scrollIndicator'>
                     <div className='scrollIndicator'></div>
                 </div>
                 <p>Dérouler</p>
             </div>
+            
         </HeroStyle>
     )
 }
