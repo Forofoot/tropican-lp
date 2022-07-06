e1a5dc56da305ac028d9dfeb48e444564522ec11
Yarline <yarlinecineus@outlook.fr>
2022-07-06 15:54:47 +0200

    legales

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import Image from "next/image"
 import Link from "next/link"
 import NewsletterSubscribe from "./NewsletterSubscribe"
 import { BsFacebook } from 'react-icons/bs';
---
+++ pages/blog/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image';
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const BlogStyle = styled.section`
+
+`
+export default function Blog({article}) {
+
+    return (
+        <BlogStyle>
+
+
+        </BlogStyle>
+    )
+}
