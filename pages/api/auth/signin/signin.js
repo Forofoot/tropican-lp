aac8c76a6023c94dd8e1c188f63c3cfe27837640
Hubert <erwan.marques@yahoo.fr>
2022-07-08 03:14:01 +0200

    Test performance

--- pages/api/auth/signin/signin.js
+++ pages/api/auth/signin/signin.js
@@ -30,14 +30,8 @@ export default async function handler(
                 return false
             }else{
                 res.status(200).json({
-                    firstName: resultGrandChildren.firstName,
-                    lastName: resultGrandChildren.lastName,
-                    avatar: resultGrandChildren.avatar,
-                    avatar_publicId: resultGrandChildren.avatar_publicId,
-                    email: resultGrandChildren.email,
                     pseudo: resultGrandChildren.pseudo,
-                    role: resultGrandChildren.role,
-                    id: resultGrandChildren.id
+                    role: resultGrandChildren.role
                 })
                 await prisma.$disconnect()
             }
@@ -49,14 +43,8 @@ export default async function handler(
                 res.status(500).json('Mot de passe ou nom d\'utilisateur incorrect')
             }else{
                 res.status(200).json({
-                    firstName: resultGrandParent.firstName,
-                    lastName: resultGrandParent.lastName,
-                    avatar: resultGrandParent.avatar,
-                    avatar_publicId: resultGrandParent.avatar_publicId,
-                    email: resultGrandParent.email,
                     pseudo: resultGrandParent.pseudo,
-                    role: resultGrandParent.role,
-                    id: resultGrandParent.id
+                    role: resultGrandParent.role
                 })
                 await prisma.$disconnect()
             }
